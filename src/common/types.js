ac8c2b11b3a99c034389304b681d92625e5fa3e9
Samuel Ventura <samuel.ventura@yeico.com>
2021-01-31 01:28:32 -0600

    Added file types

---
+++ src/common/types.js
@@ -0,0 +1,17 @@
+
+//https://www.uuidgenerator.net/
+
+const folderType = {
+  id: "8c75a529-bea0-4f07-ba35-dcb227dcd6e1",
+  name: "Folder",
+}
+
+const textFileType = {
+  id: "5e486884-685e-4641-b870-7c1ae5867326",
+  name: "Text File",
+}
+
+export default [
+  folderType,
+  textFileType,
+]
